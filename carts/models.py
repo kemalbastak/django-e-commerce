e7fd1227a2873140713406ab7ed7cc1f006c4271
Kemal Cihan Bastak <bastakkemal@gmail.com>
2021-10-12 01:01:24 +0300

    cart buttons functionality added

--- carts/models.py
+++ carts/models.py
@@ -6,14 +6,10 @@ from store.models import Product
 # Create your models here.
 class Cart(models.Model):
     cart_id = models.CharField(max_length=250, blank=True)
-    date_added = models.DateField(auto_now_add=False)
-
+    date_added = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.cart_id
-
-    def get_url(self):
-        return reverse("Cart_detail", kwargs={"pk": self.pk})
 
 
 class CartItem(models.Model):
@@ -21,6 +17,9 @@ class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
+
+    def sub_total(self):
+        return self.product.price * self.quantity
 
     def __str__(self) -> str:
         return self.product
