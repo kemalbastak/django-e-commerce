4036b15e8b5a13c71b07b6355b137f97430305db
Kemal Cihan Bastak <bastakkemal@gmail.com>
2021-10-12 13:56:55 +0300

    search bar added and some fixings

--- store/urls.py
+++ store/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', view=views.store, name='store'),
-    path('<slug:category_slug>/', view=views.store, name='products_by_category'),
-    path('<slug:category_slug>/<slug:product_slug>/', view=views.product_detail, name='product_detail'),
+    path('category/<slug:category_slug>/', view=views.store, name='products_by_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>/', view=views.product_detail, name='product_detail'),
+    path('search/', views.search, name='search')
 ]
