a83e7faa89affc11953dbda82a5e96b86913da63
Kemal Cihan Bastak <bastakkemal@gmail.com>
2021-10-12 15:32:25 +0300

    cart item variations

--- carts/models.py
+++ carts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.urls import reverse
 
-from store.models import Product
+from store.models import Product, Variation
 
 # Create your models here.
 class Cart(models.Model):
@@ -14,6 +14,7 @@ class Cart(models.Model):
 
 class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variations = models.ManyToManyField(Variation, blank=True)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
@@ -22,4 +23,4 @@ class CartItem(models.Model):
         return self.product.price * self.quantity
 
     def __str__(self) -> str:
-        return self.product
+        return f"{self.product}"
